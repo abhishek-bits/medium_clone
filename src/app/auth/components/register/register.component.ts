d4da66b15a6b5b2e7540765353275390418861e8
abhishek-bits <abhi.shar1971@gmail.com>
2023-09-23 15:09:53 +0530

    add register markup

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   // Instead of directly specifying our component
@@ -12,5 +13,24 @@ import {Component} from '@angular/core';
   // in all our components because we want to use
   // all our components without modules.
   standalone: true,
+  // We'll import ReactiveFormsModule to get the
+  // [formGroup] property for our form
+  // and specify formControlName
+  imports: [ReactiveFormsModule],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  // All our fields in the form will be defined.
+  // Here we defined all the rules for our inputs.
+  form = this.formBuilder.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  // Inject FormBuilder via constructor
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit(): void {
+    console.log('form', this.form.getRawValue());
+  }
+}
